58ace4b099232df561415c28627ab5f7cec1ee03
Philip Supruniuk <375298425457@bk.ru>
2022-09-21 12:24:22 +0300

    Initial Commit

---
+++ MyFavoritePlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFavoritePlaces
+//
+//  Created by Fill on 21.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
